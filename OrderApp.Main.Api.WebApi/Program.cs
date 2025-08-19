e58e4b033852afbc3a1d924773a80ae83fd84a3b
Ha Phi Hung <haphihung55@gmail.com>
2025-08-19 14:54:37 +0700

    feat: Set up API docs page and fixed web API ports

--- OrderApp.Main.Api.WebApi/Program.cs
+++ OrderApp.Main.Api.WebApi/Program.cs
@@ -3,8 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +14,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
