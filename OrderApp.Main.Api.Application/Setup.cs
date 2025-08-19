1eae5bc30ea546851619d6edc779b8377dcedf6c
Ha Phi Hung <haphihung55@gmail.com>
2025-08-19 21:40:43 +0700

    feat: Set up validation

---
+++ OrderApp.Main.Api.Application/Setup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
+
+namespace OrderApp.Main.Api.Application
+{
+    public static class Setup
+    {
+        public static void AddApplicationServices(this IHostApplicationBuilder builder)
+        {
+            var services = builder.Services;
+
+            services.AddFluentValidationAutoValidation();
+        }
+    }
+}
