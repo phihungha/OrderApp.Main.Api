be47739af19960006e44506c014a0b6800860ead
Ha Phi Hung <haphihung55@gmail.com>
2025-08-20 17:55:56 +0700

    feat: Setup auto converting Result objects to appropriate ActionResult in controllers

--- OrderApp.Main.Api.WebApi/Program.cs
+++ OrderApp.Main.Api.WebApi/Program.cs
@@ -1,5 +1,7 @@
+using FluentResults.Extensions.AspNetCore;
 using OrderApp.Main.Api.Application;
 using OrderApp.Main.Api.Infrastructure;
+using OrderApp.Main.Api.WebApi.ResultProfiles;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
+AspNetCoreResult.Setup(config => config.DefaultProfile = new GlobalResultProfile());
 
 builder.AddInfrastructureServices();
 builder.AddApplicationServices();
---
+++ OrderApp.Main.Api.WebApi/ResultProfiles/GlobalResultProfile.cs
@@ -0,0 +1,32 @@
+﻿using FluentResults;
+using FluentResults.Extensions.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using OrderApp.Main.Api.Application.Errors;
+
+namespace OrderApp.Main.Api.WebApi.ResultProfiles
+{
+    public class GlobalResultProfile : DefaultAspNetCoreResultEndpointProfile
+    {
+        public override ActionResult TransformFailedResultToActionResult(
+            FailedResultToActionResultTransformationContext context
+        )
+        {
+            var result = context.Result;
+
+            if (result.Errors.Count == 1 && result.HasError<NotFoundError>())
+            {
+                var error = context.Result.Errors.First();
+                return new NotFoundObjectResult(error.Message);
+            }
+
+            return base.TransformFailedResultToActionResult(context);
+        }
+
+        public override ActionResult TransformOkNoValueResultToActionResult(
+            OkResultToActionResultTransformationContext<Result> context
+        )
+        {
+            return new NoContentResult();
+        }
+    }
+}
