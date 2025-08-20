c18687f86a679fac5565c663e3e4216732c53b61
Ha Phi Hung <haphihung55@gmail.com>
2025-08-20 10:48:33 +0700

    feat: Create product-related entities

---
+++ OrderApp.Main.Api.Domain/Entities/ProductEntities/Product.cs
@@ -0,0 +1,14 @@
+﻿using OrderApp.Main.Api.Domain.Entities.StockItemEntities;
+
+namespace OrderApp.Main.Api.Domain.Entities.ProductEntities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public StockItem StockItem { get; set; } = null!;
+    }
+}
---
+++ OrderApp.Main.Api.Domain/Entities/StockItemEntities/StockItem.cs
@@ -0,0 +1,12 @@
+﻿using OrderApp.Main.Api.Domain.Entities.ProductEntities;
+
+namespace OrderApp.Main.Api.Domain.Entities.StockItemEntities
+{
+    public class StockItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+        public int Quantity { get; set; }
+    }
+}
