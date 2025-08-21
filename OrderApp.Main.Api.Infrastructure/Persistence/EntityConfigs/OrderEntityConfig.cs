9c0b0b8697e3ce8ebba28b62be0de318a7197d2a
Hà Phi Hùng <haphihung55@outlook.com>
2025-08-21 11:08:51 +0700

    feat: Configure entity relationships for order line.

---
+++ OrderApp.Main.Api.Infrastructure/Persistence/EntityConfigs/OrderEntityConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderApp.Main.Api.Domain.Entities.OrderEntities;
+
+namespace OrderApp.Main.Api.Infrastructure.Persistence.EntityConfigs
+{
+    public class OrderEntityConfig : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasMany(o => o.Products).WithMany(p => p.Orders).UsingEntity<OrderLine>();
+        }
+    }
+}
