bedff2ed1b700adbbcc782c3ab5f7f13c420fba1
Ha Phi Hung <haphihung55@gmail.com>
2025-08-27 16:59:50 +0700

    feat: Handling order fulfilment by background job and handling status updates via SQS

---
+++ OrderApp.Main.Api.Jobs/SqsMessageHandlers/OrderFulfillRequestSqsHandler.cs
@@ -0,0 +1,30 @@
+﻿using AWS.Messaging;
+using Hangfire;
+using OrderApp.Main.Api.Application.Interfaces.ApplicationServices;
+using OrderApp.Main.Api.Infrastructure.JobRequest.MessageDTOs;
+
+namespace OrderApp.Main.Api.Jobs.SqsMessageHandlers
+{
+    public class OrderFulfillRequestSqsHandler(IBackgroundJobClient backgroundJobClient)
+        : IMessageHandler<OrderFulfillRequestMessageDto>
+    {
+        private readonly IBackgroundJobClient backgroundJobClient = backgroundJobClient;
+
+        public Task<MessageProcessStatus> HandleAsync(
+            MessageEnvelope<OrderFulfillRequestMessageDto> messageEnvelope,
+            CancellationToken token = default
+        )
+        {
+            if (messageEnvelope == null || messageEnvelope.Message == null)
+            {
+                return Task.FromResult(MessageProcessStatus.Failed());
+            }
+
+            var orderId = messageEnvelope.Message.OrderId;
+
+            backgroundJobClient.Enqueue<IOrderService>(s => s.Fulfill(orderId));
+
+            return Task.FromResult(MessageProcessStatus.Success());
+        }
+    }
+}
---
+++ OrderApp.Main.Api.Jobs/SqsMessageHandlers/OrderStatusUpdateSqsHandler.cs
@@ -0,0 +1,59 @@
+﻿using AWS.Messaging;
+using FluentResults;
+using OrderApp.Main.Api.Application.Interfaces.ApplicationServices;
+
+namespace OrderApp.Main.Api.WebApi.SqsMessageHandlers
+{
+    public record OrderStatusUpdateMessageDto
+    {
+        public const string MessageType = "Order.StatusUpdate";
+
+        public enum StatusOptions
+        {
+            Shipping,
+            Shipped,
+            Completed,
+        }
+
+        public required int OrderId { get; set; }
+        public required StatusOptions Status { get; set; }
+    }
+
+    public class OrderStatusUpdateSqsHandler(IOrderService orderService)
+        : IMessageHandler<OrderStatusUpdateMessageDto>
+    {
+        private readonly IOrderService orderService = orderService;
+
+        public async Task<MessageProcessStatus> HandleAsync(
+            MessageEnvelope<OrderStatusUpdateMessageDto> messageEnvelope,
+            CancellationToken token = default
+        )
+        {
+            if (messageEnvelope == null || messageEnvelope.Message == null)
+            {
+                return MessageProcessStatus.Failed();
+            }
+
+            var orderStatus = messageEnvelope.Message.Status;
+            var orderId = messageEnvelope.Message.OrderId;
+
+            var result = orderStatus switch
+            {
+                OrderStatusUpdateMessageDto.StatusOptions.Shipping =>
+                    await orderService.BeginShipping(orderId),
+                OrderStatusUpdateMessageDto.StatusOptions.Shipped =>
+                    await orderService.FinishShipping(orderId),
+                OrderStatusUpdateMessageDto.StatusOptions.Completed => await orderService.Complete(
+                    orderId
+                ),
+                _ => Result.Fail("Invalid Status."),
+            };
+
+            if (result.IsSuccess)
+            {
+                return MessageProcessStatus.Success();
+            }
+            return MessageProcessStatus.Failed();
+        }
+    }
+}
